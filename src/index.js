452929fd3dc5067ca9cc60c08718ce12a71f36e9
Kenas Dermawan <kenas@code.id>
2021-07-08 20:43:09 +0700

    2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import App from "./App";
 import "./styles/main.scss";
